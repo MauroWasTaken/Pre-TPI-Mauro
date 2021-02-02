2ea0a6e42bff59be3a5b636c9d96a097990bb313
Mauro-Alexandre.COST <maurx18@gmail.com>
2021-02-02 16:54:11 +0100

    added the first script;

---
+++ Projet Unity/Pre-TPI Mauro/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.y <= -20.25)
+        {
+            this.gameObject.transform.position = new Vector3(0, this.gameObject.transform.position.y + 40.96f, 0);
+        }
+        this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(0, -1 * speed * Time.deltaTime, 0);
+    }
+}
